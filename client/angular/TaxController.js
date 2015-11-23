24d78e9c3a58d222a857ae9f76912b92aad8d698
brandon in <brandonin@me.com>
2015-11-23 10:38:37 -0800

    further developed the tax return

--- client/angular/TaxController.js
+++ client/angular/TaxController.js
@@ -2,39 +2,98 @@ app.controller("TaxController", function($scope) {
 	var deductionMaritalStatus = null;
 	var deductionAge = null;
 	var exemptionDependent = null;
+	var kids = false;
+	var dependentsUnder16 = 0;
+	var dependentsOver17 = 0;
+	var isDependent = false;
+	var numberOfDependents = 0;
+	var federalTaxesWithheld;
+	if(!$scope.incomeTotal) {
+		$scope.incomeTotal = 0;
+	}
 	$scope.single = function() {
-		deductionMaritalStatus = "$6,300";
+		deductionMaritalStatus = 6300;
 		$scope.deduction = deductionMaritalStatus;
 	};
 	$scope.married = function() {
-		deductionMaritalStatus = "$12,600";
+		deductionMaritalStatus = 12600;
 		$scope.deduction = deductionMaritalStatus;
 	};
 	$scope.age = function(data) {
-		if(data < 65 && ($scope.deduction == "$6,300"|| $scope.deduction == "$7,850")){
-			deductionAge = "$6,300";
-		} else if(data > 65 && $scope.deduction == "$6,300") {
-			deductionAge = "$7,850";
-		} else if(data < 65 && ($scope.deduction == "$12,600" || $scope.deduction == "$14,150")) {
-			deductionAge = "$12,600";
-		} else if(data > 65 && $scope.deduction == "$12,600") {
-			deductionAge = "$14,150";
+		if(data < 65 && ($scope.deduction == 6300|| $scope.deduction == 7850)){
+			deductionAge = 6300;
+		} else if(data > 65 && $scope.deduction == 6300) {
+			deductionAge = 7850;
+		} else if(data < 65 && ($scope.deduction == 12600 || $scope.deduction == 14150)) {
+			deductionAge = 12600;
+		} else if(data > 65 && $scope.deduction == 12600) {
+			deductionAge = 14150;
 		}
 		$scope.deduction = deductionAge;
 		console.log("on-change", $scope.deduction);
 	}
-	$scope.dependent = function(){
-		exemptionDependent = "$0.00";
+	$scope.isaDependent = function(){
+		isDependent = false;
+		$scope.isDependent = isDependent;
+		exemptionDependent = 0;
 		$scope.exemption = exemptionDependent;
+		$scope.Dependent
+		console.log($scope.exemption)
 	}
-	$scope.notDependent = function(){
-		exemptionDependent = "$4,000";
+	$scope.notaDependent = function(){
+		isDependent = true;
+		$scope.isDependent = isDependent;
+		exemptionDependent = 4000;
 		$scope.exemption = exemptionDependent;
+		console.log($scope.exemption)
 	}
-	$scope.kids = function() {
-
+	$scope.yesKids = function() {
+		kids = true;
+		$scope.kids = kids;
 	}
 	$scope.noKids = function() {
-		
+		kids = false;
+		$scope.kids = kids;
+	}
+	$scope.claimedDependents = function (data) {
+		dependentsUnder16 = data * 4000;
+		numberOfDependents = data;
+		$scope.numberOfDependents = numberOfDependents;
+		console.log(dependentsUnder16)
+		if(data){
+			$scope.exemption = dependentsUnder16 + $scope.exemption;
+			console.log("hello")
+		} else{
+			$scope.exemption = 4000;
+		}
+		console.log($scope.exemption)
+	}
+	// $scope.claimedDependentsOver17 = function (data) {
+	// 	dependentsOver17 = data * 4000;
+	// 	console.log(data)
+	// 	numberOfDependents = data;
+	// 	$scope.numberOfDependents = numberOfDependents;
+	// 	console.log(dependentsUnder16)
+	// 	if(data){
+	// 		$scope.exemption = dependentsUnder16 + $scope.exemption;
+	// 		console.log("hello")
+	// 	} else{
+	// 		$scope.exemption = 4000;
+	// 	}
+	// 	console.log($scope.exemption)
+	// }
+	$scope.totalIncome = function (data) {
+		incomeTotal = data;
+		$scope.incomeTotal = incomeTotal;
+	}
+	$scope.studentLoanInterest = function (data) {
+		studentLoanInterest = data;
+		$scope.studentInterest = studentLoanInterest;
+		console.log($scope.deduction)
+		$scope.deduction = $scope.deduction + data;
+	}
+	$scope.federalTaxesWithheld = function (data) {
+		federalTaxesWithheld = data;
+		$scope.taxesWithheld = federalTaxesWithheld;
 	}
 });
