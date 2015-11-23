740dc0a1407e40c88212c825f0e52aeb19c64e06
brandon in <brandonin@me.com>
2015-11-22 22:17:18 -0800

    added the basic logic of exemptions and deductions

--- client/angular/TaxController.js
+++ client/angular/TaxController.js
@@ -1,12 +1,40 @@
 app.controller("TaxController", function($scope) {
-	var test = null;
+	var deductionMaritalStatus = null;
+	var deductionAge = null;
+	var exemptionDependent = null;
 	$scope.single = function() {
-		test = "I'm Single!";
-		$scope.tax = test;
+		deductionMaritalStatus = "$6,300";
+		$scope.deduction = deductionMaritalStatus;
 	};
 	$scope.married = function() {
-		test = "I'm Married!";
-		$scope.tax = test;
-		console.log($scope.tax);
+		deductionMaritalStatus = "$12,600";
+		$scope.deduction = deductionMaritalStatus;
 	};
+	$scope.age = function(data) {
+		if(data < 65 && ($scope.deduction == "$6,300"|| $scope.deduction == "$7,850")){
+			deductionAge = "$6,300";
+		} else if(data > 65 && $scope.deduction == "$6,300") {
+			deductionAge = "$7,850";
+		} else if(data < 65 && ($scope.deduction == "$12,600" || $scope.deduction == "$14,150")) {
+			deductionAge = "$12,600";
+		} else if(data > 65 && $scope.deduction == "$12,600") {
+			deductionAge = "$14,150";
+		}
+		$scope.deduction = deductionAge;
+		console.log("on-change", $scope.deduction);
+	}
+	$scope.dependent = function(){
+		exemptionDependent = "$0.00";
+		$scope.exemption = exemptionDependent;
+	}
+	$scope.notDependent = function(){
+		exemptionDependent = "$4,000";
+		$scope.exemption = exemptionDependent;
+	}
+	$scope.kids = function() {
+
+	}
+	$scope.noKids = function() {
+		
+	}
 });
