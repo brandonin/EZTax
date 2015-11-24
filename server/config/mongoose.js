8159b17ad7ed1732b469e556f3231992825851f8
brandon in <brandonin@me.com>
2015-11-24 12:45:07 -0800

    fixed mongoose errors

--- server/config/mongoose.js
+++ server/config/mongoose.js
@@ -4,11 +4,11 @@ var fs = require('fs');
 // connect to the database
 mongoose.connect(process.env.MONGOLAB_URI || 'mongodb://localhost/easy_tax');
 // specify the path to all of the models
-// var models_path = __dirname + '/../models'
-// // read all of the files in the models_path and for each one check if it is a javascript file before requiring it
-// fs.readdirSync(models_path).forEach(function(file) {
-//   if(file.indexOf('.js') > 0) {
-//     require(models_path + '/' + file);
-//   }
-// })
+var models_path = __dirname + '/../models'
+// read all of the files in the models_path and for each one check if it is a javascript file before requiring it
+fs.readdirSync(models_path).forEach(function(file) {
+  if(file.indexOf('.js') > 0) {
+    require(models_path + '/' + file);
+  }
+})
 
